14ecff4b375367fd4269a7c6e9182d1d19111e90
Saloni Pathak <25113cn356@glbitm.ac.in>
2026-06-08 21:23:10 +0530

    added recursive programmed

--- day7.c/recursive.c
+++ day7.c/recursive.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 // Recursive function to calculate factorial
-long int factorial(int n)
-{
+long long factorial(int n) {
     if (n == 0 || n == 1)
         return 1;
     else
         return n * factorial(n - 1);
 }
 
-int main()
-{
+int main() {
     int num;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    printf("Factorial of %d = %ld\n", num, factorial(num));
+    if (num < 0)
+        printf("Factorial is not defined for negative numbers.\n");
+    else
+        printf("Factorial of %d = %lld\n", num, factorial(num));
 
     return 0;
-} 
+}
