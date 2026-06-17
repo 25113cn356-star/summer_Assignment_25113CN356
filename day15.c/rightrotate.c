bf359118b1ba7d70c9e64080f6729566578deec1
Saloni Pathak <25113cn356@glbitm.ac.in>
2026-06-17 20:45:55 +0530

    added rightrotate programmed

--- day15.c/rightrotate.c
+++ day15.c/rightrotate.c
@@ -7,22 +7,22 @@ int main()
     printf(" number of elements: ");
     scanf("%d", &n);
 
-    printf(" array elements:\n");
+    printf("Enter array :\n");
     for(i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
 
-    temp = a[0];
+    temp = a[n - 1];
 
-    for(i = 0; i < n - 1; i++)
+    for(i = n - 1; i > 0; i--)
     {
-        a[i] = a[i + 1];
+        a[i] = a[i - 1];
     }
 
-    a[n - 1] = temp;
+    a[0] = temp;
 
-    printf(" left rotation:\n");
+    printf("Array right rotation:\n");
     for(i = 0; i < n; i++)
     {
         printf("%d ", a[i]);
