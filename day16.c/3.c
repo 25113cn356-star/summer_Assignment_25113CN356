73377f3fd8353d384fdfaee5754a03235a9dffcd
Saloni Pathak <25113cn356@glbitm.ac.in>
2026-06-17 21:00:57 +0530

    added 3 programmed

---
+++ day16.c/3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[50], n, i, j, sum;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter sum: ");
+    scanf("%d", &sum);
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] + a[j] == sum)
+            {
+                printf("Pair = %d %d", a[i], a[j]);
+            }
+        }
+    }
+
+    return 0;
+}
