40d341f82305b04b0a119670845c04afe816e7ca
Saloni Pathak <25113cn356@glbitm.ac.in>
2026-06-28 01:19:47 +0530

    added 4 programmed

---
+++ day21.c/4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+char str[100];
+int i;
+printf("Enterstring: ");
+gets(str);
+for(i=0;str[i]!='\0';i++) {
+if(str[i]>='a'&&str[i]<='z')
+str[i]=str[i]-32;
+}
+printf("Uppercasestring = %s",str);
+return 0;
+}
