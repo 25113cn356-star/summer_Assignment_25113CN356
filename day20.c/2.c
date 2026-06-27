d6259db46cf7a840235bcce62dcb3480cb980cb8
Saloni Pathak <25113cn356@glbitm.ac.in>
2026-06-28 00:15:24 +0530

    added 2 programmed

---
+++ day20.c/2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+int n, i, j, flag = 1;
+printf(" order of the matrix: ");
+scanf("%d", &n);
+int a[n][n];
+printf("elements of the matrix:\n");
+for(i=0;i<n;i++) {
+for(j=0;j<n;j++) {
+scanf("%d", &a[i][j]);
+}
+}
+for(i=0;i<n;i++) {
+for(j=0;j<n;j++) {
+if(a[i][j]!=a[j][i]) {
+flag=0;
+break;
+}
+}
+}
+if(flag==1)
+printf("The matrix is Symmetric.");
+else
+printf("The matrix is Not Symmetric.");
+
+return 0;
+}
