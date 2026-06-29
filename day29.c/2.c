e56ff295cffd99a561f3fdaaaf2e3c7a6d30c719
Saloni Pathak <25113cn356@glbitm.ac.in>
2026-06-29 23:25:33 +0530

    added 2 programmed

---
+++ day29.c/2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() 
+{
+int arr[100], n, i, choice, sum = 0;
+printf("Enter size: ");
+scanf("%d", &n);
+printf("Enter elements:\n");
+for (i = 0; i < n; i++)
+scanf("%d", &arr[i]);
+while (1) {
+printf("\n1.Display\n2.Sum\n3.Maximum\n4.Exit\n");
+scanf("%d", &choice);
+switch (choice) {
+case 1:
+for (i = 0; i < n; i++)
+printf("%d ", arr[i]);
+printf("\n");
+break;
+case 2:
+sum = 0;
+for (i = 0; i < n; i++)
+sum += arr[i];
+printf("Sum = %d\n", sum);
+break;
+case 3: 
+{
+int max = arr[0];
+for (i = 1; i < n; i++)
+if (arr[i] > max)
+max = arr[i];
+printf("Maximum = %d\n", max);
+break;
+}
+case 4:
+return 0;
+}
+}
+}
