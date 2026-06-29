d4a34ff00b6f9c815a16a5214f64f3ffbea88b85
Saloni Pathak <25113cn356@glbitm.ac.in>
2026-06-29 23:19:55 +0530

    added 1 programmed

---
+++ day29.c/1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() 
+{
+
+ int choice;
+ float a, b;
+
+while (1) {
+printf("\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Exit\n");
+scanf("%d", &choice);
+if (choice == 5)
+break;
+printf("Enter two numbers: ");
+scanf("%f%f", &a, &b);
+switch (choice) {
+case 1:
+printf("Answer = %.2f\n", a + b);
+break;
+case 2:
+printf("Answer = %.2f\n", a - b);
+break;
+case 3:
+printf("Answer = %.2f\n", a * b);
+break;
+case 4:
+if (b != 0)
+printf("Answer = %.2f\n", a / b);
+else
+printf("Division by Zero\n");
+break;
+}
+}
+}
